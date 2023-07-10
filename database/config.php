6ae55e2c53f38634969c4dc3cc2d14ea72f10776
Ankush Pandey <software.developer.ankush@gmail.com>
2023-07-10 21:27:55 +0530

    create connection use MYSQLi

--- database/config.php
+++ database/config.php
@@ -1,3 +1,14 @@
 <?php 
+$host_name="localhost";
+$user_name="root";
+$password="";
+$db_name="core_crud_php";
 
+$conn = mysqli_connect($host_name,$user_name,$password,$db_name);
+if(!$conn)
+{
+    die("Connection failed: " . mysqli_connect_error());
+}else{
+
+}
 ?>
